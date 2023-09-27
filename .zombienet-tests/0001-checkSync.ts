9f3d475880a7a806a2d72bbc2c65b21039976d60
Nikos Kontakis <wirednkod@gmail.com>
2023-09-28 01:09:57 +0300

    add pnpm in CI

--- .zombienet-tests/0001-checkSync.ts
+++ .zombienet-tests/0001-checkSync.ts
@@ -1,16 +1,14 @@
 import { connect } from "./utils"
-import { WellKnownChain } from "../packages/sc-provider"
-import polkadotInfo from "./chain-info/polkadot"
 
 export async function run(nodeName: string, networkInfo: any) {
-  const { chainHead } = await connect(WellKnownChain.polkadot, polkadotInfo)
+  const { chainHead } = await connect(nodeName, networkInfo)
   let count = 0
 
   await new Promise(async (resolve, reject) => {
     const chainHeadFollower = chainHead(
       true,
       (event) => {
-        if (event.event === "finalized" && ++count === 2) {
+        if (event.type === "finalized" && ++count === 2) {
           resolve(chainHeadFollower.unfollow())
         }
       },
--- .zombienet-tests/0002-checkSync.ts
+++ .zombienet-tests/0002-checkSync.ts
@@ -5,12 +5,31 @@ export async function run(nodeName: string, networkInfo: any) {
   let count = 0
 
   await new Promise(async (resolve, reject) => {
+    let requested = false
     const chainHeadFollower = chainHead(
       true,
       (event) => {
-        if (event.event === "finalized" && ++count === 2) {
-          resolve(chainHeadFollower.unfollow())
+        if (event.type === "newBlock") {
+          chainHeadFollower.unpin([event.blockHash])
+          return
         }
+        if (requested || event.type !== "initialized") return
+        const latestFinalized = event.finalizedBlockHash
+        requested = true
+
+        chainHeadFollower
+          .storage(
+            latestFinalized,
+            "descendantsValues",
+            event.finalizedBlockHash,
+            null,
+          )
+          .then((some) => {
+            console.log("-------> ", some)
+            resolve(chainHeadFollower.unfollow())
+          })
+          .catch((err) => console.log("eRR", err))
+        chainHeadFollower.unpin([latestFinalized])
       },
       reject,
     )
