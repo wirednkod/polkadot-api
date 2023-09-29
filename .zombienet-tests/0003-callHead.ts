de57d78dbbaa3f7bd966c9c7ea121b2dcd3f570d
Nikos Kontakis <wirednkod@gmail.com>
2023-09-29 22:21:49 +0300

    Test

---
+++ .zombienet-tests/0003-callHead.ts
@@ -0,0 +1,39 @@
+import { Tuple, compact, metadata } from "../packages/substrate-bindings/dist"
+import { getDynamicBuilder } from "../packages/substrate-codegen/dist"
+import { connect } from "./utils"
+import { BigNumber } from "bignumber.js"
+
+const ALICE = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
+
+export async function run(_nodeName: string, networkInfo: any) {
+  const { chainHead } = await connect(networkInfo)
+
+  const opaqueMeta = Tuple(compact, metadata)
+
+  let count = 0
+
+  await new Promise(async (resolve, reject) => {
+    let requested = false
+    const chainHeadFollower = chainHead(
+      true,
+      async (event) => {
+        if (event.type === "newBlock") {
+          chainHeadFollower.unpin([event.blockHash])
+          return
+        }
+        if (requested || event.type !== "initialized") return
+        const latestFinalized = event.finalizedBlockHash
+
+        // Call metadata
+        let response = await chainHeadFollower.header(latestFinalized)
+
+        console.log("response", response)
+
+        // if (1 === 1) {
+        resolve(chainHeadFollower.unfollow())
+        // }
+      },
+      reject,
+    )
+  })
+}
